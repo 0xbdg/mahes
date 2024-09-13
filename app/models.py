e8743534d5d7806e4d28834ee5bbc7d96aa8e706
?0 <benjamindguntara@protonmail.com>
2024-09-13 12:43:32 +0700

    idjalsdjsad

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
+from datetime import datetime
 # Create your models here.
+
+class Blogs(models.Model):
+    thumbnail = models.ImageField(upload_to="thumbnail/")
+    title = models.CharField(max_length=255, unique=True)
+    content = RichTextField()
+    upload_date = models.DateTimeField(auto_now_add=datetime.now())
+
+class Registration(models.Model):
+    pass
